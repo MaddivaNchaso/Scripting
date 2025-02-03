9de65ee0c51f78036637f9652ad96b1c0ae89e2b
MaddivaNchaso <maddivanchasoo@gmail.com>
2025-02-03 18:25:29 +0000

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+
+ 
+{
+
+    //NPC Health
+    public int health = 5;
+
+    //NPC Level
+    public int level = 1;
+
+    //NPC Speed
+    public float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cantidad de vidas
+        health = health + level;
+        print("Lives" + health);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //screa la variable de nueva posicion
+        Vector3 newPosition = transform.position;
+
+        //Cambia la posición del NPC en el eje Z según la velocidad del NPC y el tiempo entre fotogramas.
+        newPosition.z += speed * Time.deltaTime;
+
+        //Nueva posicion del NPC
+        transform.position = newPosition;
+
+
+
+    }
+}
